8f560118e1101e8b6f1f0c0dd9063c785aee0e26
chodvadiyaneel <cneel111@gmail.com>
2022-05-20 16:21:11 +0530

    add to heroku

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ hbs.registerPartials(partials_path)
 app.use(express.static(static_path));
 console.log(static_path);
 
-app.get(" ",( req, res) => {
+app.get("/",( req, res) => {
     res.render('index.hbs');
 });
 
