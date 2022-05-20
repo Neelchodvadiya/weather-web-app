71ff573a0d13963189d7786b4bf237df4792ad11
chodvadiyaneel <cneel111@gmail.com>
2022-05-20 16:07:19 +0530

    make it better

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const hbs =require('hbs');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 4000;
 
 //public static path
  const static_path = path.join(__dirname,"../public");  
@@ -14,6 +14,7 @@ app.set('views',template_path);
 hbs.registerPartials(partials_path)
 
 app.use(express.static(static_path));
+console.log(static_path);
 
 app.get(" ",( req, res) => {
     res.render('index.hbs');
